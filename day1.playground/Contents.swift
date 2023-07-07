e63e66759aeebb91fe6eadae882931e953e1d312
Muhammet <134100718+Muhammetsaman@users.noreply.github.com>
2023-07-07 03:24:01 +0300

    Day 1
    .

---
+++ day1.playground/Contents.swift
@@ -0,0 +1,80 @@
+import UIKit
+
+
+
+// VERİABLES
+
+
+ print("hello world")
+
+var str = "goodbye"
+
+
+
+// STRİNG AND İNTEGERS
+
+var string = "Buraya yazı yazabilirim"
+
+var yas = "21"
+
+var populasyon = 8_000_000
+
+print(populasyon)
+
+
+// MULTİ-LİNES STRİNGS
+
+var str1 = """
+This goes
+over multiple
+lines
+"""
+
+// DOUBLE AND BOOLELANS
+
+var pi = 3.143232                       // Ondalıklı sayı
+
+var karar = true                         // True-False işlemleri
+
+
+
+// STRİNG İNTERPOLATİON
+
+var score = 85
+var strr = "your score was : \(score)"
+var results = "the test results are here : \(strr)"
+
+
+// CONSTANTS
+
+let taylor = "swift"      // Let sabitini birdaha değiştiremeyiz , var değişkenini değiştirebiliriz.
+
+
+
+// TYPE ANNOATATİONS
+
+let isim:String = "Muhammet"
+let yass:Int = 21                           // Türü bazen kendimiz belirtmemiz daha sağlıklı olabilir.
+let boy:Double = 1.81
+let calisiyor:Bool = true
+
+                    
+
+
+
+
+/*          BÖLÜM 1 ÖZET
+ 
+ 
+ 
+ Var kullanarak değişkenler ve let kullanarak sabitler oluşturursunuz. Sabitlerin mümkün olduğunca sık kullanılması tercih edilir.
+ 
+ Dizeler çift tırnakla başlar ve biter, ancak bunların birden fazla satır boyunca çalışmasını istiyorsanız, üç çift tırnak seti kullanmalısınız.
+ 
+ Tamsayılar tam sayıları, çiftler kesirli sayıları tutar ve booleanlar doğru veya yanlış tutar.
+ 
+ Dize enterpolasyonu, değerlerini dizenizin içine yerleştirerek diğer değişkenlerden ve sabitlerden dizeler oluşturmanıza olanak tanır.
+ 
+ Swift, her değişkene veya sabite bir tür atamak için tür çıkarımını kullanır, ancak isterseniz açık türler sağlayabilirsiniz.
+ 
+ */
